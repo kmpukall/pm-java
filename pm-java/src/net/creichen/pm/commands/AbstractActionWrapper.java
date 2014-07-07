fdb3e1a0d231fb159d7f39a81ec360ebc197c8cc
kmpukall <kmpukall@users.noreply.github.com>
2014-07-07 21:54:56 +0200

    Moved abstract handler code to AbstractActionWrapper

--- pm-java/src/net/creichen/pm/commands/AbstractActionWrapper.java
+++ pm-java/src/net/creichen/pm/commands/AbstractActionWrapper.java
@@ -3,6 +3,10 @@ package net.creichen.pm.commands;
 import net.creichen.pm.actions.Action;
 
 import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
 
 public abstract class AbstractActionWrapper extends AbstractHandler {
 
@@ -10,6 +14,19 @@ public abstract class AbstractActionWrapper extends AbstractHandler {
 
     protected final Action getAction() {
         return this.action;
+    }
+
+    protected ICompilationUnit getCompilationUnit(final IWorkbenchWindow window) {
+        final IWorkbenchPage activePage = window.getActivePage();
+
+        if (activePage != null) {
+            final IEditorPart editor = activePage.getActiveEditor();
+            return (ICompilationUnit) org.eclipse.jdt.ui.JavaUI.getEditorInputJavaElement(editor
+                    .getEditorInput());
+        } else {
+            return null;
+        }
+
     }
 
     protected final void setAction(final Action action) {
--- pm-java/src/net/creichen/pm/commands/SplitHandler.java
+++ pm-java/src/net/creichen/pm/commands/SplitHandler.java
@@ -10,34 +10,19 @@ import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ltk.core.refactoring.participants.RefactoringProcessor;
 import org.eclipse.ltk.ui.refactoring.RefactoringWizardOpenOperation;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class SplitHandler extends AbstractActionWrapper {
 
-    private ICompilationUnit currentICompilationUnit(final IWorkbenchWindow window) {
-        final IWorkbenchPage activePage = window.getActivePage();
-
-        if (activePage != null) {
-            final IEditorPart editor = activePage.getActiveEditor();
-
-            return (ICompilationUnit) org.eclipse.jdt.ui.JavaUI.getEditorInputJavaElement(editor
-                    .getEditorInput());
-        } else {
-            return null;
-        }
-
-    }
-
     @Override
     public final Object execute(final ExecutionEvent event) throws ExecutionException {
-        final IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
         final ISelection selection = HandlerUtil.getCurrentSelection(event);
         if (selection instanceof ITextSelection) {
 
-            final RefactoringProcessor processor = newProcessor(window, (ITextSelection) selection);
+            final IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+            final RefactoringProcessor processor = newProcessor(getCompilationUnit(window),
+                    (ITextSelection) selection);
 
             final RefactoringWizardOpenOperation operation = new RefactoringWizardOpenOperation(
                     new Wizard(processor, null));
@@ -53,9 +38,9 @@ public class SplitHandler extends AbstractActionWrapper {
         return null;
     }
 
-    private RefactoringProcessor newProcessor(final IWorkbenchWindow window,
+    private RefactoringProcessor newProcessor(final ICompilationUnit compilationUnit,
             final ITextSelection selection) {
-        return new SplitProcessor(selection, currentICompilationUnit(window));
+        return new SplitProcessor(selection, compilationUnit);
     }
 
 }
