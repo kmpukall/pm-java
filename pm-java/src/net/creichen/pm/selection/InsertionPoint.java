6d30a1d8cdeba658dddd4f7a3d2eaef134644601
kmpukall <kmpukall@gmail.com>
2014-07-12 23:20:10 +0200

    Continued refactoring of InsertionPoint further.

--- pm-java/src/net/creichen/pm/selection/InsertionPoint.java
+++ pm-java/src/net/creichen/pm/selection/InsertionPoint.java
@@ -11,136 +11,137 @@ package net.creichen.pm.selection;
 
 import static net.creichen.pm.utils.APIWrapperUtil.getStructuralProperty;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.jdt.core.dom.*;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Range;
+
 // This class is a mess
 
 public class InsertionPoint {
 
-    private final CompilationUnit compilationUnit;
-    private final int offset;
-    private int insertionIndex;
-    private ChildListPropertyDescriptor insertionProperty;
-    private ASTNode insertionParent;
+	private final CompilationUnit compilationUnit;
+	private int insertionIndex;
+	private ChildListPropertyDescriptor insertionProperty;
+	private ASTNode insertionParent;
 
-    public InsertionPoint(final CompilationUnit compilationUnit, final int offset) {
-        this.insertionIndex = -1;
-        this.compilationUnit = compilationUnit;
-        this.offset = offset;
+	public InsertionPoint(final CompilationUnit compilationUnit, final int offset) {
+		this.insertionIndex = -1;
+		this.compilationUnit = compilationUnit;
 
-        /*
-         * a point is an insertion point if:
-         * 
-         * - it is in a block or a type declaration - it is NOT in a child of the above - OR it is
-         * the first/last character of such a child
-         */
+		/*
+		 * a point is an insertion point if:
+		 * 
+		 * - it is in a block or a type declaration - it is NOT in a child of
+		 * the above - OR it is the first/last character of such a child
+		 */
 
-        final ASTNode node = new SurroundingNodeFinder(offset).findOn(this.compilationUnit);
-        if (node != null) {
-            final List<ASTNode> statements = getStatements(node);
-            final int statementCount = statements.size();
-            if (statementCount == 0 || this.offset <= statements.get(0).getStartPosition()) {
-                this.insertionIndex = 0;
-            } else {
-                final ASTNode lastStatement = statements.get(statementCount - 1);
+		final ASTNode node = new SurroundingNodeFinder(offset).findOn(this.compilationUnit);
+		if (node != null) {
+			final List<Range<Integer>> ranges = rangesBetween(getStatements(node));
+			for (int i = 0; i < ranges.size() && this.insertionIndex < 0; i++) {
+				if (ranges.get(i).contains(offset)) {
+					this.insertionIndex = i;
+				}
+			}
+		}
+		if (isValid()) {
+			this.insertionParent = node;
+			this.insertionProperty = getProperty(node);
+		}
+	}
 
-                if (this.offset >= lastStatement.getStartPosition() + lastStatement.getLength()) {
-                    this.insertionIndex = statementCount;
-                } else {
-                    // offset is not before the first statement, or after the
-                    // last statement, so see if it is between two statements
+	private List<Range<Integer>> rangesBetween(final List<ASTNode> statements) {
+		final ArrayList<Range<Integer>> ranges = new ArrayList<Range<Integer>>();
+		if (statements.isEmpty()) {
+			// in this case, any cursor position within the parent node is valid
+			final Range<Integer> unboundedRange = Range.all();
+			ranges.add(unboundedRange);
+		} else {
+			ranges.add(Range.atMost(statements.get(0).getStartPosition()));
+			for (int i = 0; i < statements.size() - 1; i++) {
+				final ASTNode current = statements.get(i);
+				final ASTNode next = statements.get(i + 1);
+				ranges.add(Range.closed(current.getStartPosition() + current.getLength(),
+						next.getStartPosition()));
+			}
+			final ASTNode lastStatement = Iterables.getLast(statements);
+			ranges.add(Range.atLeast(lastStatement.getStartPosition() + lastStatement.getLength()));
+		}
+		return ranges;
+	}
 
-                    for (int i = 1; i < statementCount; i++) {
-                        final ASTNode statement1 = statements.get(i - 1);
-                        final ASTNode statement2 = statements.get(i);
+	public int getInsertionIndex() {
+		return this.insertionIndex;
+	}
 
-                        if (this.offset >= statement1.getStartPosition() + statement1.getLength()
-                                && this.offset <= statement2.getStartPosition()) {
+	public ASTNode getInsertionParent() {
+		return this.insertionParent;
+	}
 
-                            this.insertionIndex = i;
-                            break;
-                        }
-                    }
-                }
-            }
+	public ChildListPropertyDescriptor getInsertionProperty() {
+		return this.insertionProperty;
+	}
 
-            if (isValid()) {
-                this.insertionParent = node;
-                this.insertionProperty = getProperty(node);
-            }
-        }
-    }
+	public boolean isValid() {
+		return this.insertionIndex != -1;
+	}
 
-    public int getInsertionIndex() {
-        return this.insertionIndex;
-    }
+	private List<ASTNode> getStatements(final ASTNode parentNode) {
+		switch (parentNode.getNodeType()) {
+			case ASTNode.BLOCK:
+			case ASTNode.TYPE_DECLARATION:
+				final ChildListPropertyDescriptor property = getProperty(parentNode);
+				return getStructuralProperty(property, parentNode);
+			default:
+				return Collections.emptyList();
+		}
 
-    public ASTNode getInsertionParent() {
-        return this.insertionParent;
-    }
+	}
 
-    public ChildListPropertyDescriptor getInsertionProperty() {
-        return this.insertionProperty;
-    }
+	private ChildListPropertyDescriptor getProperty(final ASTNode node) {
+		if (node.getNodeType() == ASTNode.TYPE_DECLARATION) {
+			return TypeDeclaration.BODY_DECLARATIONS_PROPERTY;
+		} else {
+			return Block.STATEMENTS_PROPERTY;
+		}
+	}
 
-    public boolean isValid() {
-        return this.insertionIndex != -1;
-    }
+	private static final class SurroundingNodeFinder extends ASTVisitor {
+		private final int position;
+		private ASTNode containingNode;
 
-    private List<ASTNode> getStatements(final ASTNode parentNode) {
-        switch (parentNode.getNodeType()) {
-            case ASTNode.BLOCK:
-            case ASTNode.TYPE_DECLARATION:
-                final ChildListPropertyDescriptor property = getProperty(parentNode);
-                return getStructuralProperty(property, parentNode);
-            default:
-                return Collections.emptyList();
-        }
+		private SurroundingNodeFinder(final int position) {
+			this.position = position;
+		}
 
-    }
+		@Override
+		public boolean visit(final Block node) {
+			return visitInternal(node);
+		}
 
-    private ChildListPropertyDescriptor getProperty(final ASTNode node) {
-        if (node.getNodeType() == ASTNode.TYPE_DECLARATION) {
-            return TypeDeclaration.BODY_DECLARATIONS_PROPERTY;
-        } else {
-            return Block.STATEMENTS_PROPERTY;
-        }
-    }
+		@Override
+		public boolean visit(final TypeDeclaration node) {
+			return visitInternal(node);
+		}
 
-    private static final class SurroundingNodeFinder extends ASTVisitor {
-        private final int position;
-        private ASTNode containingNode;
+		private boolean visitInternal(final ASTNode node) {
+			if (node.getStartPosition() < this.position
+					&& this.position < node.getStartPosition() + node.getLength()) {
+				this.containingNode = node;
+				return true;
+			}
+			return false;
+		}
 
-        private SurroundingNodeFinder(final int position) {
-            this.position = position;
-        }
-
-        @Override
-        public boolean visit(final Block node) {
-            return visitInternal(node);
-        }
-
-        @Override
-        public boolean visit(final TypeDeclaration node) {
-            return visitInternal(node);
-        }
-
-        private boolean visitInternal(final ASTNode node) {
-            if (node.getStartPosition() < this.position
-                    && this.position < node.getStartPosition() + node.getLength()) {
-                this.containingNode = node;
-                return true;
-            }
-            return false;
-        }
-
-        public ASTNode findOn(final ASTNode node) {
-            final SurroundingNodeFinder finder = new SurroundingNodeFinder(this.position);
-            node.accept(finder);
-            return finder.containingNode;
-        }
-    }
+		public ASTNode findOn(final ASTNode node) {
+			final SurroundingNodeFinder finder = new SurroundingNodeFinder(this.position);
+			node.accept(finder);
+			return finder.containingNode;
+		}
+	}
 }
