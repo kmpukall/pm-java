6ed305ce52bd25e3131a8216111887ad266a82eb
kmpukall <kmpukall@gmail.com>
2014-07-13 16:34:07 +0200

    Refactored and reactivated tests.

--- pm-java/test/net/creichen/pm/selection/SelectionTest.java
+++ pm-java/test/net/creichen/pm/selection/SelectionTest.java
@@ -9,130 +9,158 @@
 
 package net.creichen.pm.selection;
 
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 import net.creichen.pm.tests.PMTest;
 
 import org.eclipse.jdt.core.dom.*;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class SelectionTest extends PMTest {
 
+	// TODO: these fail when toCompilationUnit is used. find out why this
+	// matters
+
 	@Test
 	public void testNoneSaneSelection() {
-		final String source = "class S {void f() {int x,y; f(); x++;} }";
-		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(source, "S.java");
+		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(
+				"class S {void f() {int x,y; f(); x++;} }", "S.java");
 
-		final Selection selectionObject = new Selection(compilationUnit, 59 - 26, 61 - 59);
+		final Selection selectionObject = new Selection(compilationUnit,
+				startingAfter("class S {void f() {int x,y; f(); "), selecting("x+"));
 
-		assertThat(selectionObject.singleSelectedNode(), is(nullValue()));
-		assertFalse(selectionObject.isSaneSelection());
+		assertThat(selectionObject.singleSelectedNode(), is(nullValue(ASTNode.class)));
+		assertThat(selectionObject.isSaneSelection(), is(false));
 	}
 
 	@Test
-	public void testSelectMemberDeclaration() {
-		final String source = "class S {int x,y; void f(int i) {int x,y; f(x); } int z; }";
-		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(source, "S.java");
+	public void testSelectMemberDeclaration1() {
+		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(
+				"class S {int x,y; void f(int i) {int x,y; f(x); } int z; }", "S.java");
 
-		Selection selectionObject = new Selection(compilationUnit, 35 - 26, 43 - 35);
+		final Selection selectionObject = new Selection(compilationUnit,
+				startingAfter("class S {"), selecting("int x,y;"));
 
-		ASTNode selectedNode = selectionObject.singleSelectedNode();
-		assertTrue(selectedNode instanceof FieldDeclaration);
+		assertThat(selectionObject.singleSelectedNode(), is(instanceOf(FieldDeclaration.class)));
+	}
 
-		selectionObject = new Selection(compilationUnit, 44 - 26, 75 - 44);
+	@Test
+	public void testSelectMemberDeclaration2() {
+		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(
+				"class S {int x,y; void f(int i) {int x,y; f(x); } int z; }", "S.java");
 
-		selectedNode = selectionObject.singleSelectedNode();
-		assertTrue(selectedNode instanceof MethodDeclaration);
+		final Selection selectionObject = new Selection(compilationUnit,
+				startingAfter("class S {int x,y; "), selecting("void f(int i) {int x,y; f(x); }"));
 
-		selectionObject = new Selection(compilationUnit, 76 - 26, 82 - 76);
+		assertThat(selectionObject.singleSelectedNode(), is(instanceOf(MethodDeclaration.class)));
+	}
 
-		selectedNode = selectionObject.singleSelectedNode();
-		assertTrue(selectedNode instanceof FieldDeclaration);
+	@Test
+	public void testSelectMemberDeclaration3() {
+		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(
+				"class S {int x,y; void f(int i) {int x,y; f(x); } int z; }", "S.java");
+
+		final Selection selectionObject = new Selection(compilationUnit,
+				startingAfter("class S {int x,y; void f(int i) {int x,y; f(x); } "),
+				selecting("int z;"));
+
+		assertThat(selectionObject.singleSelectedNode(), is(instanceOf(FieldDeclaration.class)));
 	}
 
 	@Test
 	public void testSelectMemberDeclarations() {
-		final String source = "class S {int x; void f(int i) {int x,y; f(x); x++; } int y;}";
-		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(source, "S.java");
+		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(
+				"class S {int x; void f(int i) {int x,y; f(x); x++; } int y;}", "S.java");
 
-		final Selection selectionObject = new Selection(compilationUnit, 35 - 26, 79 - 35);
+		final Selection selectionObject = new Selection(compilationUnit,
+				startingAfter("class S {"),
+				selecting("int x; void f(int i) {int x,y; f(x); x++; } "));
 
-		assertNull(selectionObject.singleSelectedNode());
-		assertTrue(selectionObject.selectedNodeParent() instanceof TypeDeclaration);
-		assertEquals(TypeDeclaration.BODY_DECLARATIONS_PROPERTY,
-				selectionObject.selectedNodeParentProperty());
-		assertEquals(0, selectionObject.selectedNodeParentPropertyListOffset());
-		assertEquals(2, selectionObject.selectedNodeParentPropertyListLength());
-		assertTrue(selectionObject.isListSelection());
-		assertTrue(selectionObject.isMultipleSelection());
+		assertThat(selectionObject.singleSelectedNode(), is(nullValue(ASTNode.class)));
+		assertThat(selectionObject.selectedNodeParent(), is(instanceOf(TypeDeclaration.class)));
+		assertThat(selectionObject.selectedNodeParentProperty(),
+				is((StructuralPropertyDescriptor) TypeDeclaration.BODY_DECLARATIONS_PROPERTY));
+		assertThat(selectionObject.selectedNodeParentPropertyListOffset(), is(0));
+		assertThat(selectionObject.selectedNodeParentPropertyListLength(), is(2));
+		assertThat(selectionObject.isListSelection(), is(true));
+		assertThat(selectionObject.isMultipleSelection(), is(true));
 
 	}
 
 	@Test
 	public void testSelectMethodInvocation() {
-		final String source = "class S {void f(int i) {int x,y; f(x); } }";
-		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(source, "S.java");
+		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(
+				"class S {void f(int i) {int x,y; f(x); } }", "S.java");
 
-		final Selection selectionObject = new Selection(compilationUnit, 59 - 26, 4);
+		final Selection selectionObject = new Selection(compilationUnit,
+				startingAfter("class S {void f(int i) {int x,y; "), selecting("f(x);"));
 
-		final ASTNode selectedNode = selectionObject.singleSelectedNode();
-		assertTrue(selectedNode != null);
-		assertTrue(selectedNode instanceof MethodInvocation);
+		assertThat(selectionObject.singleSelectedNode(), is(instanceOf(ASTNode.class)));
 	}
 
 	@Test
-	@Ignore
 	public void testSelectSimpleName() {
-		final String source = "class S {void f() {int x,y; f(); x++;} }";
-		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(source, "S.java");
+		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(
+				"class S {void f() {int x,y; f(); x++;} }", "S.java");
 
-		final Selection selectionObject = new Selection(compilationUnit, 49 - 26, 1);
+		final Selection selectionObject = new Selection(compilationUnit,
+				startingAfter("class S {void f() {int "), selecting("x"));
 
-		assertTrue(selectionObject.singleSelectedNode() instanceof SimpleName);
-		assertFalse(selectionObject.isSaneSelection());
+		assertThat(selectionObject.singleSelectedNode(), is(instanceOf(SimpleName.class)));
+		assertThat(selectionObject.isSaneSelection(), is(true));
 
 	}
 
 	@Test
 	public void testSelectStatement() {
-		final String source = "class S {int x,y; int f() {int x,y; while(1) {x = 5; y = x +1;} } }";
-		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(source, "S.java");
+		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(
+				"class S {int x,y; int f() {int x,y; while(1) {x = 5; y = x +1;} } }", "S.java");
 
-		final Selection whileSelection = new Selection(compilationUnit, 62 - 26, 89 - 62);
+		final Selection whileSelection = new Selection(compilationUnit,
+				startingAfter("class S {int x,y; int f() {int x,y; "),
+				selecting("while(1) {x = 5; y = x +1;}"));
 
-		final ASTNode selectedNode = whileSelection.singleSelectedNode();
-		assertTrue(selectedNode != null);
-		assertTrue(selectedNode instanceof WhileStatement);
+		assertThat(whileSelection.singleSelectedNode(), is(instanceOf(WhileStatement.class)));
 	}
 
 	@Test
 	public void testSelectStatements() {
-		final String source = "class S {void f(int i) {int x,y; f(x); x++; } }";
-		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(source, "S.java");
+		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(
+				"class S {void f(int i) {int x,y; f(x); x++; } }", "S.java");
 
-		final Selection selectionObject = new Selection(compilationUnit, 59 - 26, 69 - 59);
+		final Selection selectionObject = new Selection(compilationUnit,
+				startingAfter("class S {void f(int i) {int x,y; "), selecting("f(x); x++;"));
 
-		assertNull(selectionObject.singleSelectedNode());
-		assertTrue(selectionObject.selectedNodeParent() instanceof Block);
-		assertEquals(Block.STATEMENTS_PROPERTY, selectionObject.selectedNodeParentProperty());
-		assertEquals(1, selectionObject.selectedNodeParentPropertyListOffset());
-		assertEquals(2, selectionObject.selectedNodeParentPropertyListLength());
-		assertTrue(selectionObject.isListSelection());
-		assertTrue(selectionObject.isMultipleSelection());
+		assertThat(selectionObject.singleSelectedNode(), is(nullValue(ASTNode.class)));
+		assertThat(selectionObject.selectedNodeParent(), is(instanceOf(Block.class)));
+		assertThat(selectionObject.selectedNodeParentProperty(),
+				is((StructuralPropertyDescriptor) Block.STATEMENTS_PROPERTY));
+		assertThat(selectionObject.selectedNodeParentPropertyListOffset(), is(1));
+		assertThat(selectionObject.selectedNodeParentPropertyListLength(), is(2));
+		assertThat(selectionObject.isListSelection(), is(true));
+		assertThat(selectionObject.isMultipleSelection(), is(true));
 
 	}
 
 	@Test
 	public void testSelectStatementWithSurroundingWhitespace() {
-		final String source = "class S {void f() {int x,y; f(); x++;} }";
-		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(source, "S.java");
+		final CompilationUnit compilationUnit = parseCompilationUnitFromSource(
+				"class S {void f() {int x,y; f(); x++;} }", "S.java");
 
-		final Selection selectionObject = new Selection(compilationUnit, 54 - 26, 59 - 54);
+		final Selection selectionObject = new Selection(compilationUnit,
+				startingAfter("class S {void f() {int x,y;"), selecting(" f(); "));
 
-		final ASTNode selectedNode = selectionObject.singleSelectedNode();
-		assertTrue(selectedNode instanceof Statement);
+		assertThat(selectionObject.singleSelectedNode(), is(instanceOf(Statement.class)));
+	}
+
+	private int selecting(final String selection) {
+		return selection.length();
+	}
+
+	private int startingAfter(final String prefix) {
+		return prefix.length();
 	}
 
 }
