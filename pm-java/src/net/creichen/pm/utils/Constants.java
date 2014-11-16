968489cd7ef0f25f4046914071402673ab914ab9
Kai-Marian Pukall <kmpukall@gmail.com>
2014-11-16 13:44:35 +0100

    Added Constants class.

---
+++ pm-java/src/net/creichen/pm/utils/Constants.java
@@ -0,0 +1,12 @@
+package net.creichen.pm.utils;
+
+public final class Constants {
+
+    private Constants() {
+
+    }
+
+    public static final boolean VISIT_CHILDREN = true;
+    public static final boolean SKIP_CHILDREN = false;
+
+}
